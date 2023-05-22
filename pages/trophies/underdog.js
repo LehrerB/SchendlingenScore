1c515d2e6033f942bb9edb623a5a5d660bbc08a6
lawtrafalgar02 <46304349+lawtrafalgar02@users.noreply.github.com>
2023-05-22 19:25:55 +0200

    underdogs
    who let the underdogs out?
    - included way to set upper limit, so small underdog and big underdog don't include the same links twice sometimes

--- pages/trophies/underdog.js
+++ pages/trophies/underdog.js
@@ -1,26 +1,27 @@
 
-function wonVsHigherElo(diff) {
-  return function(game) {
+function wonVsHigherElo(diff1, diff2) { //returns true if opponent is higher between range
+  return function(game) {               //ignores diff2 if it's smaller than diff1 (no upper limit)
     let plElo = parseInt(game.isWhite ? game.header().WhiteElo : game.header().BlackElo)
     let opElo = parseInt(game.isWhite ? game.header().BlackElo : game.header().WhiteElo)
-    return plElo + diff <= opElo;
+  if(diff1 < diff2) {return (plElo + diff1 <= opElo)&&!(plElo + diff2 <= opElo)}
+  return plElo + diff1 <= opElo;
   }
 }
 
 export let small_underdog = {
   title: 'Small underdog',
   description: 'Gewinne gegen einen Spieler, der mindestens 40 ELO mehr hat.',
-  check: wonVsHigherElo(40)
+  check: wonVsHigherElo(40,120)
 }
 
 export let middle_underdog = {
   title: 'Underdog',
   description: 'Gewinne gegen einen Spieler, der mindestens 80 ELO mehr hat.',
-  check: wonVsHigherElo(80)
+  check: wonVsHigherElo(80,120)
 }
 
 export let big_underdog = {
   title: 'Big underdog',
   description: 'Gewinne gegen einen Spieler, der mindestens 120 ELO mehr hat.',
-  check: wonVsHigherElo(120)
+  check: wonVsHigherElo(120,0)
 }
